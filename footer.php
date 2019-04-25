05ee4e6c55a00e035419135aaa241b5a184b3c30
Stijn Wingens <stijnwingens140@gmail.com>
2019-04-25 13:18:06 +0200

    Fix footer

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
-        <p>&copy;2019 Groepje I28 <a href="#">Algemene voorwaarden</a> <a href="#">FAQ</a> </p>
-<div class="uk-container uk-container-small uk-margin-bottom uk-text-center uk-position-botom">
+<div class="uk-container uk-container-small uk-margin-bottom uk-text-center uk-postition-absolute">
+    <p>&copy;2019 Groep I28
+        <a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="file-text"></span> Algemene voorwaarden</a>
+        <a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="question"></span> FAQ</a>
+    </p>
 </div>
