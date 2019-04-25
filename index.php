ade6a93748b699f613fc4aa4b07f1f214746b989
Stijn Wingens <stijnwingens140@gmail.com>
2019-04-25 16:26:13 +0200

    create item generation

--- index.php
+++ index.php
@@ -76,159 +76,9 @@
 
     </div>
 
-    <div class="uk-container uk-width-3-5 uk-section">
-        <h2>Highlights</h2>
-        <hr>
-        <div id="highlight_producten">
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="uk-container uk-width-3-5 uk-section">
-        <h2>Rubriek</h2>
-        <hr>
-        <div id="highlight_producten">
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="uk-container uk-width-3-5 uk-section">
-        <h2>Rubriek</h2>
-        <hr>
-        <div id="highlight_producten">
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
+    <?php echo get_section('Highlights');?>
+    <?php echo get_section('Rubriek');?>
+    <?php echo get_section('Rubriek');?>
 
     <div class="uk-card uk-card-default uk-card-title uk-width-1-5 uk-position-center-right">
         <div class="uk-card-header">
@@ -256,3 +106,40 @@
 </body>
 
 </html>
+
+<?php 
+    //functions
+    function get_section($section_name){
+        $html = '
+            <div class="uk-container uk-width-3-5 uk-section">
+                <h2>'.$section_name.'</h2>
+                <hr>
+                <div class="items">'
+                .get_items($section_name).'
+                </div>
+            </div>';
+        return $html;
+    }
+
+    function get_items($item_type){
+        $html = '
+            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-body">Item</div>
+                </div>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-body">Item</div>
+                </div>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-body">Item</div>
+                </div>
+            </div>
+        ';
+        
+        for($i = 0; $i < 2; $i++){
+            $html.=$html;
+        }
+
+        return $html;
+    }
+?>
