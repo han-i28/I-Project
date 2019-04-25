c07fff9b6813275edcb10cbcb8ad2ccf8a3b1f4f
Stijn Wingens <stijnwingens140@gmail.com>
2019-04-25 10:05:41 +0200

    fix bug in insertscript

--- Createscript EA1+2.sql
+++ Createscript EA1+2.sql
@@ -38,7 +38,7 @@ VALUES
 (1, 'OPEL ASTRA TE KOOP', 'Mooie opel astra te koop, zo goed als nieuw. Hij gaat weg omdat ik het niet meer nodig heb.', 
 200.00, 'iDeal', 'Nijmegen', 'Nederland', '2019-04-19 16:00:00', 0, 'Ophalen', 'Johan de Vries', '2019-04-25 16:00:00', 0),
 (2, 'Gitaar', 'Deze gitaar van mijn zoon wordt niet meer gebruikt dus mag ie weg, het is een koopje.', 15.00, 'PayPal',
-'Arnhem', 'Nederland', '2019-03-20 15:00:00', 0, 'Ophalen', 'Richard Klaassen', '2019-04-1915:00:00', 0),
+'Arnhem', 'Nederland', '2019-03-20 15:00:00', 0, 'Ophalen', 'Richard Klaassen', '2019-04-19 15:00:00', 0),
 (3, 'PlayStation 4', 'Heb een gamepc gekocht.', 150.00, 'Contant',
 'Enschede', 'Nederland', '2019-02-14 15:00:00', 0, 'Ophalen', 'Tony te Vrede', '2019-04-14 15:00:00', 0),
 (4, 'Konijn', 'Dit konijn staat te koop omdat het agressief is, het heeft al 2 hokken kapot gebeten.', 5.00, 'Contant', 'Delft', 
