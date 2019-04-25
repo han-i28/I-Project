ebb5d333b874b1dd7a15989d604b02eb0bf21f5a
Stijn Wingens <stijnwingens140@gmail.com>
2019-04-25 21:15:15 +0200

    create functions.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     require 'header.php';
+    require_once 'functions.php';
 ?>
 
 <html>
@@ -73,7 +74,6 @@
                 </ul>
             </li>
         </ul>
-
     </div>
 
     <?php echo get_section('Highlights');?>
@@ -106,41 +106,3 @@
 </body>
 
 </html>
-
-<?php 
-    //functions
-    function get_section($section_name){
-        $html = '
-            <div class="uk-container uk-width-3-5 uk-section">
-                <h2>'.$section_name.'</h2>
-                <hr>
-                <div class="items">'
-                    .get_items($section_name).'
-                </div>
-            </div>';
-        return $html;
-    }
-
-    function get_items($item_type){
-        $html = '';
-        $html_part = '
-            <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-                <div>
-                    <div class="uk-card uk-card-default uk-card-body">Item</div>
-                </div>
-            </div>
-        ';
-        
-        for($i = 0; $i < 2; $i++){
-            $html.=$html_part;
-        }
-
-        return $html;
-    }
-?>
