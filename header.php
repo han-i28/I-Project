6c2edcc72f20e58ac428f261a24f658accbbd5d8
IvanTeunesen <ivanteunesen@hotmail.com>
2019-04-26 11:23:46 +0200

    header form now has a method

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <h1 class="uk-heading-bullet">Eenmaal Andermaal</h1>
     <nav class="uk-navbar-container" uk-navbar>
         <div class="uk-navbar-left">
-            <form class="uk-search uk-search-navbar" action="#">
+            <form class="uk-search uk-search-navbar" action="zoeken.php" method="post">
                 <span uk-search-icon></span>
-                <input class="uk-search-input" type="search" placeholder="Zoeken..." />
+                <input class="uk-search-input" name="search" type="search" placeholder="Zoeken..." />
             </form>
         </div>
         <div class="uk-navbar-right">
