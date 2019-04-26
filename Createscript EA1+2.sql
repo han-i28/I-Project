7fcd05efd47c868507192d8930f9a30b41978b24
Stijn Wingens <stijnwingens140@gmail.com>
2019-04-26 13:18:22 +0200

    add FK_Voorwerp_land_id and FK_Gebruiker_land_id

--- Createscript EA1+2.sql
+++ Createscript EA1+2.sql
@@ -27,8 +27,8 @@ CREATE TABLE voorwerp(
 	startprijs NUMERIC(19, 7) NOT NULL,
 	betalingswijze CHAR(25) NOT NULL,
 	betalingsinstructie CHAR(25) NULL,
-	plaatsnaam VARCHAR(255) NOT NULL,--!!!
-	land CHAR(50) NOT NULL,--!!!
+	plaatsnaam VARCHAR(255) NOT NULL,
+	land_id SMALLINT NOT NULL,
 	looptijdBegin DATETIME NOT NULL,
 	verzendkosten NUMERIC(19, 7) NOT NULL, 
 	verzendinstructies CHAR(27) NOT NULL,
@@ -40,17 +40,16 @@ CREATE TABLE voorwerp(
 	CONSTRAINT PK_Voorwerpnummer PRIMARY KEY (voorwerpnummer)
 )
 
-
 CREATE TABLE gebruiker (
 	gebruikersnaam CHAR(20) NOT NULL,
 	voornaam CHAR(255) NOT NULL,
 	tussenvoegsel VARCHAR(10) NULL,
 	achternaam CHAR(255) NOT NULL,
-	adresregel_1 VARCHAR(60) NOT NULL,
+	adresregel_1 VARCHAR(60) NOT NULL,--Street, addressnumber
 	adresregel_2 VARCHAR (60) NULL,
 	postcode CHAR(16) NOT NULL,--Montserrat zipcodes are 13 chars long: future proof with 16
 	plaatsnaam CHAR(85) NOT NULL,
-	land CHAR(255) NOT NULL,
+	land_id SMALLINT NOT NULL,
 	geboortedatum DATE NOT NULL,
 	telefoon CHAR(15) NOT NULL,
 	mailbox VARCHAR(50) NOT NULL,--Create email check
@@ -61,3 +60,18 @@ CREATE TABLE gebruiker (
 	CONSTRAINT PK_Gebruiker PRIMARY KEY (gebruikersnaam)
 )
 
+GO
+
+ALTER TABLE voorwerp
+	ADD CONSTRAINT FK_Voorwerp_land_id FOREIGN KEY (land_id)
+		REFERENCES country (id)
+		ON UPDATE NO ACTION
+		ON DELETE NO ACTION
+;
+
+ALTER TABLE gebruiker
+	ADD CONSTRAINT FK_Gebruiker_land_id FOREIGN KEY (land_id)
+		REFERENCES country (id)
+		ON UPDATE NO ACTION
+		ON DELETE NO ACTION
+;
