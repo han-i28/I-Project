fc4e7a84083315bfbb8e0bd6da2c7fc6787302ba
IvanTeunesen <ivanteunesen@hotmail.com>
2019-05-06 13:21:13 +0200

    Zoek & +-filterfuntie

--- zoeken.php
+++ zoeken.php
@@ -3,13 +3,72 @@ session_start();
 include 'dbconnection.php';
 include 'header.php';
 
-$search = $_POST['search'];
-$_SESSION['search'] = $search;
+if (isset($_GET['search'])){
+    $search = $_GET['search'];
+    $_SESSION['search'] = $search;
 
-$sql = "SELECT * FROM voorwerp WHERE titel like (?)";
-$query = $dbh->prepare($sql);
-$query->execute(array($_SESSION['search']));
+    $sqlsearch = "SELECT * FROM voorwerp WHERE titel like (?)";
+    $querysearch = $dbh->prepare($sqlsearch);
+    $querysearch->execute(array($search));
+    $resultssearch = $querysearch->fetch(PDO::FETCH_ASSOC);
+}
 
-$results = $query->fetch(PDO::FETCH_ASSOC);
+if(isset($_GET['minprice'])){
+    if(isset($_GET['maxprice'])){
+        $minprice = $_GET['minprice'];
+        $_SESSION['minprice'] = $minprice;
+        $maxprice = $_GET['maxprice'];
+        $_SESSION['maxprice'] = $maxprice;
 
-var_dump($results);
+        $sql = "SELECT * FROM voorwerp WHERE startprijs > :minprice AND startprijs < :maxprice";
+        $query = $dbh->prepare($sql);
+        $query->execute(array(':minprice'=>$minprice, ':maxprice'=>$maxprice));
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eenmaal Andermaal | Zoekresultaten</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.2/css/uikit.min.css" />
+    <link rel="stylesheet" href="style.css">
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.2/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.2/js/uikit-icons.min.js"></script>
+
+</head>
+    <body>
+        <!--Filters-->
+        <div class="uk-width-1-4">
+            <form action="zoeken.php" method="get">
+                <div class="uk-grid-small uk-grid-margin" uk-grid>
+                <div class="uk-width-1-5">
+                        <legend class="uk-legend">&euro;</legend>
+                    </div>
+                    <div class="uk-width-1-5">
+                        <input type="text" name="minprice" id="minprice" class="uk-input" placeholder="10">
+                    </div>
+                    <div class="uk-width-1-5">
+                        <legend class="uk-legend"> tot: </legend>
+                    </div>
+                    <div class="uk-width-1-5">
+                            <input type="text" name="maxprice" id="maxprice" class="uk-input" placeholder="100">                
+                    </div>
+                    </div>
+                    <div class="uk-width-1-5">
+                        <button type="submit" class="uk-button-primary"><span uk-icon="icon: chevron-right"></span></button>
+                    </div>                
+                </div>                    
+            </form>
+        </div>
+        <!--Laat de resultaten zien-->
+        <div class="uk-container" id="resultaten">
+        
+        </div>
+    </body>
+</html>
