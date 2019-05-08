3a64dbe18ef2585fb36425a5eb1ca0eb06d044c8
IvanTeunesen <ivanteunesen@hotmail.com>
2019-05-08 10:32:57 +0200

    part of the faq

---
+++ faq.php
@@ -0,0 +1,92 @@
+<?php
+
+include 'header.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Eenmaal Andermaal | FAQ</title>
+
+         <!-- UIkit JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.2/js/uikit.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.2/js/uikit-icons.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.2/css/uikit.min.css" />
+    </head>
+    <body>
+        <h1 class="uk-text-center">
+            Frequently asked questions
+        </h1>
+
+        <div class="uk-container">
+            <ul uk-accordion>
+                <li>
+                    <a href="#" class="uk-accordion-title">Hoe registreer ik op deze site?</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            U kunt registreren op deze site door op het menuknopje te klikken, daarna op de registratie link en dan uw gegevens
+                            in te vullen. U krijgt na het invullen van uw gegevens een bevestigingsmail. Door deze te bevestigen is uw account aangemaakt.
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <a href="#" class="uk-accordion-title">Hoe log ik in op deze site?</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Wanneer u een account geregistreerd en bevestigd heeft kunt u inloggen. Dit doet u door op het menuknopje te klikken en daarna op 
+                            inloggen. Vul hier uw gegevens in en klik op de knop. U bent nu ingelogd.
+                        </p>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#" class="uk-accordion-title">Hoe plaats ik producten op de site?</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Zodra u geregistreerd, ingelogd en uw account een verkoopaccount gemaakt heeft kunt u producten aanbieden.
+                            Dit doet u doormiddel van de 'Nieuw product' knop op uw accountpagina. Daarna kunt u uw product zijn details invullen en 
+                            een afbeelding uploaden.
+                        </p>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#" class="uk-accordion-title">Moet ik een account hebben om te kunnen bieden?</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Ja, het is vereist dat elke bieder een account heeft. Dit is zo voor de veiligheid en zekerheid voor de verkoper.
+                        </p>
+                    </div>
+                </li>
+
+                <li>
+                    <a href="#" class="uk-accordion-title">Kost het geld om producten aan te bieden?</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Nee, dit is gratis.
+                        </p>
+                    </div>                    
+                </li>
+
+                <li>
+                    <a href="#" class="uk-accordion-title">Is er een leeftijdslimiet?</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Nee, op deze site mag je met elke leeftijd bezoeken, registreren en bieden.
+                        </p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        
+    </body>
+</html>
+
+<?php
+    include 'footer.php';
+?>
--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<div class="uk-container uk-container-small uk-margin-bottom uk-text-center uk-postition-absolute uk-flex uk-flex-wrap">
+<div class="uk-container uk-container-small uk-margin-bottom uk-margin-top uk-text-center uk-postition-absolute uk-flex uk-flex-wrap">
     <p>&copy;2019 Groep I28
         <a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="file-text"></span> Algemene voorwaarden</a>
-        <a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="question"></span> FAQ</a>
+        <a href="faq.php" class="uk-margin-left"><span class="uk-icon" uk-icon ="question"></span> FAQ</a>
     </p>
 </div>
