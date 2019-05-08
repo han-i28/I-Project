d30ca25c2b6a90c293873e52824f0ea8244f32f2
Mike <48909795+mikevh17@users.noreply.github.com>
2019-05-08 14:44:09 +0200

    Update Controller.php
    removed unused variable

--- Core/Controller.php
+++ Core/Controller.php
@@ -1,8 +1,6 @@
 <?php
-    class Controller
-    {
+    class Controller {
         var $vars = [];
-        var $layout = "default";
 
         function set($d) {
             $this->vars = array_merge($this->vars, $d);
