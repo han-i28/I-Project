3e1164059dcf70055fdc15283b55d4735954efd9
Stijn Wingens <stijnwingens140@gmail.com>
2019-05-08 15:23:11 +0200

    merge mvc into homepage

--- footer.php
+++
@@ -1,6 +0,0 @@
-<div class="uk-container uk-container-small uk-margin-bottom uk-text-center uk-postition-absolute uk-flex uk-flex-wrap">
-    <p>&copy;2019 Groep I28
-    <div class="uk-text-middle"><a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="file-text"></span> Algemene voorwaarden</a></div>
-    <div class="uk-text-right"><a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="question"></span> FAQ</a></div>
-    </p>
-</div>
