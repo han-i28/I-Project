2cfb8d1069ee2990a3a1588a0fc277654c60442a
Stijn Wingens <stijnwingens140@gmail.com>
2019-05-09 13:24:42 +0200

    put logo in header

--- Views/Includes/header.inc.php
+++ Views/Includes/header.inc.php
@@ -1,7 +1,3 @@
-<div class="uk-flex uk-flex-center">
-    <a href="index.php"> <img src="afb/logo.png" alt="Logo"></a>
-</div>
-    
 <nav class="uk-navbar-container" uk-navbar="dropbar: true;" uk-sticky="bottom: #offset">
     <div class="uk-navbar-left">
         <ul class="uk-navbar-nav">
@@ -58,6 +54,9 @@
                 </div>
             </li>
         </ul>
+    </div>
+    <div class="uk-navbar-center uk-navbar-small">
+        <a href="index.php"> <img src="afb/logo.png" alt="Logo"></a>
     </div>
     <div class="uk-navbar-right uk-margin-right uk-margin-top">
         <form action="" class="uk-flex">
