74c4cff4e69eae67b9dde1dcc1eb33a9acd30351
jasperjacobs <49537790+jasperjacobs@users.noreply.github.com>
2019-05-09 14:55:58 +0200

    Revert "Merge branch 'homepage' of https://github.com/han-i28/I-Project into homepage"
    This reverts commit 9eb30170ca3850c3df238219fe95fca63bc463b4, reversing
    changes made to a3f1fc6cf54ac6553d6f23eb18f6d5b4927d5ed6.

--- Views/Includes/head.inc.php
+++ Views/Includes/head.inc.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/css/uikit.min.css" />
+    <link rel="stylesheet" type="text/css" href="/I-Project/assets/css/uikit.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.2/js/uikit.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.2/js/uikit-icons.min.js"></script>
 
