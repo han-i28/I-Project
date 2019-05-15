bd72233c8bcba70014fca9f3f594e1e9e7c0105f
pepijnwinkelmolen <49524435+pepijnwinkelmolen@users.noreply.github.com>
2019-05-15 12:58:21 +0200

    session hijacking update

--- Views/Includes/footer.inc.php
+++ Views/Includes/footer.inc.php
@@ -1,4 +1,6 @@
-<div class="uk-container uk-flex uk-flex-center uk-visible@s" id="footer">
+<hr class="uk-divider-small">
+
+<div class="uk-container uk-flex uk-flex-center uk-position-bottom uk-visible@s" id="footer">
     <p>&copy;2019 Groep I28
     <div class="uk-text-middle"><a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="file-text"></span> Algemene voorwaarden</a></div>
     <div class="uk-text-right"><a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="question"></span> FAQ</a></div>
