e91de04177cffa61d4477e9a0d957961f240e93b
Stijn Wingens <stijnwingens140@gmail.com>
2019-05-17 11:44:43 +0200

    Create 08 - CREATE feedback.sql

---
+++ 08 - CREATE feedback.sql
@@ -0,0 +1,20 @@
+USE EA
+GO
+
+CREATE TABLE feedback (
+	voorwerp NUMERIC(14) NOT NULL,
+	soort_gebruiker CHAR(8) NOT NULL,
+	feedbacksoort CHAR(8) NOT NULL,
+	moment DATETIME NOT NULL,
+	commentaar VARCHAR(600) NULL,
+	CONSTRAINT PK_Feedback_voorwerp_soortGebruiker PRIMARY KEY (voorwerp, soort_gebruiker),
+	CONSTRAINT FK_Feedback_voorwerp FOREIGN KEY (voorwerp)
+		REFERENCES voorwerp (voorwerpnummer)
+		ON UPDATE CASCADE
+		ON DELETE NO ACTION
+)
+GO
+
+INSERT INTO feedback (voorwerp, soort_gebruiker, feedbacksoort, moment, commentaar)
+VALUES
+(1, 'Koper', 'Positief', '11-05-2019', 'De auto is met een volle tank afgeleverd en was netjes schoongemaakt. Ik ben er erg blij mee!')
