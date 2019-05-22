583dc0a360b800668869a1fc1649a086ae9e4fbf
Stijn Wingens <stijnwingens140@gmail.com>
2019-05-22 16:47:06 +0200

    Fix footer position to bottom

--- Views/Includes/footer.inc.php
+++ Views/Includes/footer.inc.php
@@ -1,11 +1,11 @@
-<div class="uk-container uk-flex uk-flex-center uk-visible@s" id="footer">
+<div class="uk-container uk-flex uk-flex-center uk-visible@s uk-position-bottom" id="footer">
     <p>&copy;2019 Groep I28
     <div class="uk-text-middle"><a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="file-text"></span> Algemene voorwaarden</a></div>
     <div class="uk-text-right"><a href="#" class="uk-margin-left"><span class="uk-icon" uk-icon ="question"></span> FAQ</a></div>
     </p>
 </div>
 
-<div class="uk-container uk-flex uk-flex-center uk-flex-column uk-hidden@s uk-text-center" id="footer">
+<div class="uk-container uk-flex uk-flex-center uk-flex-column uk-hidden@s uk-text-center uk-position-bottom" id="footer">
     <p class="uk-margin-top">&copy;2019 Groep I28
     <div ><a href="#"><span class="uk-icon" uk-icon ="file-text"></span> Algemene voorwaarden</a></div>
     <div ><a href="#"><span class="uk-icon" uk-icon ="question"></span> FAQ</a></div>
