5bad1aea559bf1303d3aac0db09f144f3535b7f7
Stijn Wingens <stijnwingens140@gmail.com>
2019-05-23 18:26:51 +0200

    Update 09 - CREATE bod.sql

--- 09 - CREATE bod.sql
+++ 09 - CREATE bod.sql
@@ -1,14 +1,14 @@
 USE iproject28
 GO
 
+SELECT * FROM biedingen
+
 CREATE TABLE bod (
 	voorwerp BIGINT NOT NULL,
-	bodBedrag CHAR(10) NOT NULL,
+	bodBedrag NUMERIC(19,7) NOT NULL,
 	gebruiker VARCHAR(20) NOT NULL,
-	bodMoment DATETIME NOT NULL
+	bodMoment DATETIME NOT NULL DEFAULT GETDATE()
 	CONSTRAINT PK_Bod_voorwerp_bodBedrag PRIMARY KEY (voorwerp, bodBedrag),
-	CONSTRAINT UNQ_Bod_gebruiker_bodMoment UNIQUE (gebruiker, bodMoment),
-	CONSTRAINT UNQ_Bod_voorwerp_bodMoment UNIQUE (gebruiker, bodMoment),
 	CONSTRAINT FK_Bod_voorwerp FOREIGN KEY (voorwerp)
 		REFERENCES voorwerp (voorwerpnummer)
 		ON UPDATE CASCADE
@@ -16,7 +16,11 @@ CREATE TABLE bod (
 	CONSTRAINT FK_Bod_gebruiker FOREIGN KEY (gebruiker)
 		REFERENCES gebruiker (gebruikersnaam)
 		ON UPDATE NO ACTION
-		ON DELETE NO ACTION
+		ON DELETE NO ACTION,
+	CONSTRAINT UNQ_Bod_gebruiker_bodMoment UNIQUE (gebruiker, bodMoment),
+	CONSTRAINT UNQ_Bod_voorwerp_bodMoment UNIQUE (gebruiker, bodMoment),
+	CONSTRAINT CHK_Bod_hoger_bod CHECK (bodBedrag > (SELECT MAX(bodBedrag) FROM bod)),
+	CONSTRAINT CHK_Bod_bodMoment_volgorde CHECK (bodMoment > (SELECT MAX(bodMoment) FROM bod))
 )
 GO
 
