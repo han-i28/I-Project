4599b692ac97c63d32cfc5487ab0a7feccee03ab
Michel Bodde <49525449+MichelBodde@users.noreply.github.com>
2019-05-27 16:16:10 +0200

    Update loginModel.php
    - added functions for verification
    - updated user authentication to take verification-status into account.

--- model/loginModel.php
+++ model/loginModel.php
@@ -3,9 +3,23 @@
 class loginModel extends Model {
 
     public function getUserAuthentication($username) {
-        $sql = "SELECT gebruikersnaam, wachtwoord FROM gebruiker WHERE gebruikersnaam = :gebruikersnaam";
+        $sql = "SELECT gebruikersnaam, wachtwoord, is_geverifieerd FROM gebruiker WHERE gebruikersnaam = :gebruikersnaam";
         $req = Database::getBdd()->prepare($sql);
         $req->execute(array(':gebruikersnaam' => $username));
+        return $req->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getVkeyCheck($vkey) {
+        $sql = "SELECT is_geverifieerd, vkey FROM gebruiker WHERE is_geverifieerd = 0 AND vkey=:vkey";
+        $req = Database::getBdd()->prepare($sql);
+        $req->execute(array(':vkey' => $vkey));
+        return $req->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function setVerification($vkey) {
+        $sql = "UPDATE gebruiker SET is_geverifieerd = 1 WHERE vkey=:vkey";
+        $req = Database::getBdd()->prepare($sql);
+        $req->execute(array(':vkey' => $vkey));
         return $req->fetch(PDO::FETCH_ASSOC);
     }
 }
