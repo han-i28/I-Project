3128de3f36aed273c4c0f6fc75cfb601220ba66e
Stijn Wingens <stijnwingens140@gmail.com>
2019-05-28 16:01:42 +0200

    Use secure_form() on login

--- controller/loginController.php
+++ controller/loginController.php
@@ -4,56 +4,57 @@ class loginController extends Controller {
     function index() {
         if(!isset($_SESSION['loggedIn'])) {
             if (isset($_POST['login_submit'])) {
-                    $mailuid = secure_input((isset($_POST['gebruikersnaam']) ? $_POST['gebruikersnaam'] : null));
+                
+                $this->secure_form($_POST);
+                $mailuid = (isset($_POST['gebruikersnaam']) ? $_POST['gebruikersnaam'] : null);
+                $password = (isset($_POST['wachtwoord']) ? $_POST['wachtwoord'] : null);
 
-                    $password = secure_input((isset($_POST['wachtwoord']) ? $_POST['wachtwoord'] : null));
+                if (empty($mailuid) || empty($password)) {
+                    $data['error_input'] = "empty_fields";
+                    header("Location: ../login?error=emptyfields&gebruikersnaam=".$mailuid);
+                    exit();
+                } else {
+                    require(PATH . '/model/loginModel.php');
+                    $loginModel = new loginModel();
 
-                    if (empty($mailuid) || empty($password)) {
-                        $data['error_input'] = "empty_fields";
-                        header("Location: ../login?error=emptyfields&gebruikersnaam=".$mailuid);
+                    $resultArray = $loginModel->getUserAuthentication($mailuid);
+                    if (empty($resultArray)) {
+                        $data['error_input'] = "wrong_input";
+                        header("Location: ../login?error=wronguidpwd");
                         exit();
                     } else {
-                        require(PATH . '/model/loginModel.php');
-                        $loginModel = new loginModel();
+                        $pwdCheck = password_verify($password, $resultArray['wachtwoord']);
 
-                        $resultArray = $loginModel->getUserAuthentication($mailuid);
-                        if (empty($resultArray)) {
+                        if ($pwdCheck == false) {
                             $data['error_input'] = "wrong_input";
                             header("Location: ../login?error=wronguidpwd");
                             exit();
-                        } else {
-                            $pwdCheck = password_verify($password, $resultArray['wachtwoord']);
+                        } elseif ($resultArray['is_geverifieerd'] == 0) {
+                            $data['error_input'] = "not_verified";
+                            header("Location: ../login?error=notverified");
+                            exit();
+                        } else if ($pwdCheck == true && $resultArray['is_geverifieerd'] == 1) {
 
-                            if ($pwdCheck == false) {
-                                $data['error_input'] = "wrong_input";
-                                header("Location: ../login?error=wronguidpwd");
-                                exit();
-                            } elseif ($resultArray['is_geverifieerd'] == 0) {
-                                $data['error_input'] = "not_verified";
-                                header("Location: ../login?error=notverified");
-                                exit();
-                            } else if ($pwdCheck == true && $resultArray['is_geverifieerd'] == 1) {
+                            ini_set('session.cookie_httponly', true); //Preventie van session hijacking door cookies niet door javascript te kunnen sturen
 
-                                ini_set('session.cookie_httponly', true); //Preventie van session hijacking door cookies niet door javascript te kunnen sturen
+                            session_start();
 
-                                session_start();
-
-                                if (isset($_SESSION['last_ip']) === false) {
-                                    $_SESSION['last_ip'] = $_SERVER['REMOTE_ADDR'];
-                                }
-
-                                $_SESSION['gebruikersnaam'] = $resultArray['gebruikersnaam'];
-                                $_SESSION['loggedIn'] = true;
-                                $_SESSION['time'] = time();//success
-
-
-                                unset($mailuid);
-                                unset($password);
-                                unset($resultArray);
-                                header('Location: ' . SITEURL . '');
+                            if (isset($_SESSION['last_ip']) === false) {
+                                $_SESSION['last_ip'] = $_SERVER['REMOTE_ADDR'];
                             }
+
+                            $_SESSION['gebruikersnaam'] = $resultArray['gebruikersnaam'];
+                            $_SESSION['loggedIn'] = true;
+                            $_SESSION['time'] = time();//success
+
+
+                            unset($mailuid);
+                            unset($password);
+                            unset($resultArray);
+                            header('Location: ' . SITEURL . '');
                         }
                     }
+                }
 
             }
 
