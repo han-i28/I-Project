024565e31218d8beea70405deef49a06e503f2ee
Stijn Wingens <stijnwingens140@gmail.com>
2019-05-28 14:53:01 +0200

    Use icons in registration form

--- view/userregistreren.php
+++ view/userregistreren.php
@@ -56,10 +56,12 @@ if (isset($_GET["registration"])) {
     <div class="uk-grid" uk-grid>
         <form action="" method="POST" class="uk-form-horizontal uk-width-1-1 uk-margin-large" >
             <h1 class="uk-margin-top uk-width-1-1 uk-text-center">Registratie</h1>
+            <p class="uk-width-1-1 uk-text-center">Velden met het <span class="uk-icon" uk-icon="icon: chevron-double-left"></span> icoon zijn verplicht.</p>
             <div class="uk-width-1-1 uk-margin-top">
                 <label class="uk-form-label" for="gebruikersnaam">Gebruikersnaam</label>
                 <div class="uk-form-controls">
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: user"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="gebruikersnaam" id="gebruikersnaam" type="text" maxlength="20" placeholder="JanvdH12...">
                     </div>
@@ -69,6 +71,7 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="voornaam">Voornaam</label>
                 <div class="uk-form-controls" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: info"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="voornaam" id="voornaam" type="text" maxlength="255" placeholder="Jan...">
                     </div>
@@ -77,13 +80,17 @@ if (isset($_GET["registration"])) {
             <div class="uk-width-1-1"> <!--uk-width-1-3@s-->
                 <label class="uk-form-label" for="tussenvoegsel">Tussenvoegsel</label>
                 <div class="uk-form-controls" >
-                    <input class="uk-input" name="tussenvoegsel" id="tussenvoegsel" type="text" maxlength="10" placeholder="Van der...">
-                </div>
+                    <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: info"></span>
+                        <input class="uk-input" name="tussenvoegsel" id="tussenvoegsel" type="text" maxlength="10" placeholder="Van der...">
+                    </div>
+                   </div>
             </div>
             <div class="uk-width-1-1"> <!--uk-width-1-3@s-->
                 <label class="uk-form-label" for="achternaam">Achternaam</label>
                 <div class="uk-form-controls" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: info"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="achternaam" id="achternaam" type="text" maxlength="255" placeholder="Heijden...">
                     </div>
@@ -93,6 +100,7 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="adres_1">Adres 1</label>
                 <div class="uk-form-controls" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: home"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="adres 1" id="adres_1" type="text" maxlength="60" placeholder="Straatnaam 123...">
                     </div>
@@ -101,13 +109,17 @@ if (isset($_GET["registration"])) {
             <div class="uk-width-1-1"> <!--uk-width-1-2@s-->
                 <label class="uk-form-label" for="adres_2">Adres 2</label>
                 <div class="uk-form-controls" >
-                    <input class="uk-input" name="adres 2" id="adres_2" type="text" maxlength="60" placeholder="Straatnaam 456...">
+                    <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: home"></span>
+                        <input class="uk-input" name="adres 2" id="adres_2" type="text" maxlength="60" placeholder="Straatnaam 456...">
+                    </div>
                 </div>
             </div>
             <div class="uk-width-1-1"> <!--uk-width-1-3@s-->
                 <label class="uk-form-label" for="postcode">Postcode</label>
                 <div class="uk-form-controls" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: location"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="postcode" id="postcode" type="text" maxlength="16" placeholder="1234AB...">
                     </div>
@@ -117,6 +129,7 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="plaatsnaam">Plaatsnaam</label>
                 <div class="uk-form-controls" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: location"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="plaatsnaam" id="plaatsnaam" type="text" maxlength="85" placeholder="Amsterdam...">
                     </div>
@@ -126,6 +139,7 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="geboortedatum">Geboortedatum</label>
                 <div class="uk-form-controls" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: calendar"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="geboortedatum" id="geboortedatum" type="date" placeholder="Geboortedatum...">
                     </div>
@@ -135,6 +149,7 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="telefoonnummer">Telefoonnummer</label>
                 <div class="uk-form-controls" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: receiver"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="telefoonnummer" id="telefoonnummer" type="tel" maxlength="15" placeholder="+31 6 12345678..." pattern="((?:\+|00)[17](?: |\-)?|(?:\+|00)[1-9]\d{0,2}(?: |\-)?|(?:\+|00)1\-\d{3}(?: |\-)?)?(0\d|\([0-9]{3}\)|[1-9]{0,3})(?:((?: |\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\-)[0-9]{3}(?: |\-)[0-9]{4})|([0-9]{7}))">
                     </div>
@@ -144,6 +159,7 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="mailbox">Mailbox</label>
                 <div class="uk-form-controls" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: mail"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="mailbox" id="mailbox" type="email" maxlength="50" placeholder="naam@adres.com...">
                     </div>
@@ -153,6 +169,7 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="wachtwoord">Wachtwoord</label>
                 <div class="uk-form-controls" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="wachtwoord" id="wachtwoord" type="password" maxlength="255" placeholder="W@chtw00rd123... (minimaal 8 tekens)">
                     </div>
@@ -162,6 +179,7 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="wachtwoord_bevestigen">Wachtwoord bevestigen</label>
                 <div class="uk-form-controls">
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="wachtwoord bevestigen" id="wachtwoord_bevestigen" type="password" maxlength="255" placeholder="W@chtw00rd123... (minimaal 8 tekens)">
                     </div>
@@ -171,7 +189,7 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="beveiligingsvraag">Beveiligingsvraag</label>
                 <div uk-form-custom="target: > * > span:first-child" >
                     <select class="uk-select uk-width-1-1" name="beveiligingsvraag" id="beveiligingsvraag">										<!-- HIER MOET NOG EEN LIJST VAN VRAGEN UIT DE DATABASE KOMEN VIA FUNCTION getVragenList() -->
-                        <option selected disabled>Kies een beveiligingsvraag</option>
+                        <option selected disabled><span class="uk-form-icon" uk-icon="icon: question"></span>Kies een beveiligingsvraag</option>
                         <?php echo $this->vars['vragen']; ?>
                     </select>
                     <button class="uk-button uk-button-default" type="button" tabindex="-1">
@@ -183,13 +201,13 @@ if (isset($_GET["registration"])) {
                 <label class="uk-form-label" for="antwoordtekst">Antwoordtekst</label>
                 <div class="uk-form-controls uk-margin-top" >
                     <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: pencil"></span>
                         <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                         <input class="uk-input" name="antwoordtekst" id="antwoordtekst" type="text" maxlength="20" placeholder="Antwoord...">
                     </div>
                 </div>
             </div>
-            <button class="uk-button uk-button-primary uk-margin-top uk-text-middle uk-width-1-1" type="submit" name="signup_submit">Registreren</button>
-		    <p>Velden met <span class="uk-icon" uk-icon="icon: chevron-double-left"></span> zijn verplicht.</p>
+            <button class="uk-button uk-button-primary uk-margin-top uk-text-middle uk-width-1-1" type="submit" name="signup_submit">Registreren <span class="uk-icon" uk-icon="icon: cloud-upload"></span></button>
         </form>
     </div>
 </div>
