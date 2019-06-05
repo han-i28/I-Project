4278431c36bb1317975673d5d1661429fdb44ce5
Michel Bodde <49525449+MichelBodde@users.noreply.github.com>
2019-06-05 16:49:33 +0200

    Update aanbiedenController.php
    login nodig om te werken door session variabelen.
    begin errors

--- controller/aanbiedenController.php
+++ controller/aanbiedenController.php
@@ -4,110 +4,115 @@ class aanbiedenController extends Controller
 {
     function index()
     {
-       // if (!isset($_SESSION['loggedIn'])) {//if logged in return exit registration
+        if (!isset($_SESSION['loggedIn'])) {//if logged in return exit registration
             require(PATH . '/model/aanbiedenModel.php');
 
             if (isset($_POST['aanbieden_submit'])) {//when submitted
                 $this->secure_form($_POST);//secure the form
 
-                $aanbiedenModel = new aanbiedenModel();
+                $errors = $this->checkForErrors();//check for invalid inputs
 
-                $resultArrayVerkoper = $aanbiedenModel->getVerkoper($_SESSION['gebruikersnaam']);
-                $resultArrayVoorwerp = $aanbiedenModel->getVoorwerpnummer();
+                if(!$errors) {
+                    $aanbiedenModel = new aanbiedenModel();
 
-                if (empty($resultArrayVerkoper)) {
-                    $data['error_input'] = "wrong_input";
-                    header("Location: ../aanbieden");
-                    exit();
-                } elseif(empty($resultArrayVoorwerp)) {
-                    $data['error_input'] = "wrong_input";
-                    header("Location: ../aanbieden");
-                    exit();
-                } else {
-                    $voorwerpnummer = $resultArrayVoorwerp['voorwerpnummer'] + 1;
-                    $looptijdBegin = date("Y-m-d H:i:s");
-                    $tempTijd = strtotime($looptijdBegin);
-                    if ($_POST['looptijd'] == '3') {
-                        $tempTijd = strtotime("+3 day", $tempTijd);
-                    } elseif ($_POST['looptijd'] == '5') {
-                        $tempTijd = strtotime("+5 day", $tempTijd);
-                    } elseif ($_POST['looptijd'] == '7') {
-                        $tempTijd = strtotime("+7 day", $tempTijd);
-                    } else {
-                        $data['error_input'] = "wrong_input";
+                    $resultArrayVerkoper = $aanbiedenModel->getVerkoper($_SESSION['gebruikersnaam']);
+                    $resultArrayVoorwerp = $aanbiedenModel->getVoorwerpnummer();
+
+                    if (empty($resultArrayVerkoper)) {
+                        $data['error'] = "no_user_found";
                         header("Location: ../aanbieden");
                         exit();
-                    }
-                    $looptijdEinde = date("Y-m-d H:i:s", $tempTijd);
-
-                    $this->createVeiling($voorwerpnummer, $resultArrayVerkoper['postcode'], $resultArrayVerkoper['plaatsnaam'], $resultArrayVerkoper['GBA_CODE'], $looptijdBegin, $resultArrayVerkoper['gebruikersnaam'], $looptijdEinde);
-                }
-
-                $count = 0;
-                $target_dir = "Assets/uploads/";
-
-                foreach ($_FILES['fileToUpload']['name'] as $filename) {
-                    $size = ($_FILES['fileToUpload']['size']);
-                    $temp = $target_dir;
-                    $tmp = $_FILES['fileToUpload']['tmp_name'][$count];
-                    $count = $count + 1;
-                    $temp = $temp . basename($filename);
-                    move_uploaded_file($tmp, $temp);
-                    $temp = '';
-                    $tmp = '';
-
-
-                    $target_file = $target_dir . basename($filename);
-                    $uploadOk = 1;
-                    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-
-                    if (isset($_POST["submit"])) {
-                        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-                        if ($check !== false) {
-                            echo "File is an image - " . $check["mime"] . ".";
-                            $uploadOk = 1;
+                    } elseif (empty($resultArrayVoorwerp)) {
+                        $data['error'] = "sql_error";
+                        header("Location: ../aanbieden");
+                        exit();
+                    } else {
+                        $voorwerpnummer = $resultArrayVoorwerp['voorwerpnummer'] + 1;
+                        $looptijdBegin = date("Y-m-d H:i:s");
+                        $tempTijd = strtotime($looptijdBegin);
+                        if ($_POST['looptijd'] == '3') {
+                            $tempTijd = strtotime("+3 day", $tempTijd);
+                        } elseif ($_POST['looptijd'] == '5') {
+                            $tempTijd = strtotime("+5 day", $tempTijd);
+                        } elseif ($_POST['looptijd'] == '7') {
+                            $tempTijd = strtotime("+7 day", $tempTijd);
                         } else {
-                            echo "File is not an image.";
-                            $uploadOk = 0;
+                            $data['error'] = "sql_error";
+                            header("Location: ../aanbieden");
+                            exit();
+                        }
+                        $looptijdEinde = date("Y-m-d H:i:s", $tempTijd);
+
+                        $this->createVeiling($voorwerpnummer, $resultArrayVerkoper['postcode'], $resultArrayVerkoper['plaatsnaam'], $resultArrayVerkoper['GBA_CODE'], $looptijdBegin, $resultArrayVerkoper['gebruikersnaam'], $looptijdEinde);
+                    }
+
+                    $count = 0;
+                    $target_dir = "Assets/uploads/";
+
+                    foreach ($_FILES['fileToUpload']['name'] as $filename) {
+                        $size = ($_FILES['fileToUpload']['size']);
+                        $temp = $target_dir;
+                        $tmp = $_FILES['fileToUpload']['tmp_name'][$count];
+                        $count = $count + 1;
+                        $temp = $temp . basename($filename);
+                        move_uploaded_file($tmp, $temp);
+                        $temp = '';
+                        $tmp = '';
+
+
+                        $target_file = $target_dir . basename($filename);
+                        $uploadOk = 1;
+                        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+                        // Check if image file is a actual image or fake image
+
+                        if (isset($_POST["submit"])) {
+                            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+                            if ($check !== false) {
+                                echo "File is an image - " . $check["mime"] . ".";
+                                $uploadOk = 1;
+                            } else {
+                                echo "File is not an image.";
+                                $uploadOk = 0;
+                            }
                         }
                     }
-                }
-// Check if file already exists
-                /*if (file_exists($target_file)) {
-                    echo "sorry, het bestand bestaat al.";
-                    $uploadOk = 0;
-                }*/
-// Check file size
-//if ($size > 50000000) {
-//  echo "Sorry, het bestand is te groot.";
-// $uploadOk = 0;
-//}
-// Allow certain file formats
-                if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                    && $imageFileType != "gif") {
-                    echo "Sorry, alleen JPG, JPEG, PNG & GIF bestanden zijn toegestaan.";
-                    $uploadOk = 0;
-                }
-// Check if $uploadOk is set to 0 by an error
-                if ($uploadOk == 0) {
-                    echo "Sorry, you foto was niet geupload.";
-// if everything is ok, try to upload file
-                }/* else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, er was een fout tijdens het uploaden.";
-    }
-}*/
-
+                    // Check if file already exists
+                    /*if (file_exists($target_file)) {
+                        echo "sorry, het bestand bestaat al.";
+                        $uploadOk = 0;
+                    }*/
+                    // Check file size
+                    //if ($size > 50000000) {
+                    //  echo "Sorry, het bestand is te groot.";
+                    // $uploadOk = 0;
+                    //}
+                    // Allow certain file formats
+                    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+                        && $imageFileType != "gif") {
+                        echo "Sorry, alleen JPG, JPEG, PNG & GIF bestanden zijn toegestaan.";
+                        $uploadOk = 0;
+                    }
+                    // Check if $uploadOk is set to 0 by an error
+                    if ($uploadOk == 0) {
+                        echo "Sorry, you foto was niet geupload.";
+                        // if everything is ok, try to upload file
+                    }/* else {
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+            } else {
+                echo "Sorry, er was een fout tijdens het uploaden.";
             }
-        $data['title'] = "Eenmaal Andermaal - Aanbieden";
-        $data['page'] = "aanbieden";
-        $this->set($data);
-        $this->load_view("template");
+        }*/
+                } else {
+                    header("location: " . SITEURL . "");
+                }
+            }
+            $data['title'] = "Eenmaal Andermaal - Aanbieden";
+            $data['page'] = "aanbieden";
+            $this->set($data);
+            $this->load_view("template");
         }
-    //}
+    }
 
     private function createVeiling($voorwerpnummer, $postcode, $plaatsnaam, $GBA_CODE, $looptijdBegin, $verkoper, $looptijdEinde){
         $aanbiedenModel = new aanbiedenModel();
@@ -119,16 +124,22 @@ class aanbiedenController extends Controller
         $aanbiedenModel->setVeiling($user_data);
     }
 
-//    private function checkForErrors($registratieModel){
-//        if (!isset($_POST['titel']) || !isset($_POST['beschrijving']) || !isset($_POST['startprijs']) || !isset($_POST['verzendinstructies']) || !isset($_POST['betalingswijze']) || !isset($_POST['looptijd']) || !isset($_POST['conditie'])) {
-//            $data['error_input'] = "empty_fields";
-//        } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $_POST['titel'])) { //				titel
-//            $data['error_input'] = "invalid_titel";
-//        } elseif (!preg_match("/^[a-zA-Z0-9-]*$/", $_POST['beschrijving'])) { //						beschrijving
-//            $data['error_input'] = "invalid_beschrijving";
-//        } else (!preg_match("/^[a-zA-Z0-9 -]*$/", $_POST['startprijs'])) { //				startprijs
-//            $data['error_input'] = "invalid_startprijs";
-//        }
-//        return empty($data['error_input']);//if no errors returns true
-//    }
+    private function checkForErrors(){
+        if (!isset($_POST['titel']) || !isset($_POST['beschrijving']) || !isset($_POST['startprijs']) || !isset($_POST['verzendinstructies']) || !isset($_POST['betalingswijze']) || !isset($_POST['looptijd']) || !isset($_POST['conditie'])) {
+            $data['error_input'] = "empty_fields";
+        } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $_POST['titel'])) { //				titel
+            $data['error_input'] = "invalid_titel";
+        } elseif (!preg_match("/^[a-zA-Z0-9-]*$/", $_POST['beschrijving'])) { //						beschrijving
+            $data['error_input'] = "invalid_beschrijving";
+        } elseif (!preg_match("/^[a-zA-Z0-9 -]*$/", $_POST['startprijs'])) { //				startprijs
+            $data['error_input'] = "invalid_startprijs";
+        } elseif (!preg_match("/^[a-zA-Z0-9-]*$/", $_POST['betalingsinstructie'])) { //						betalingsinstructie
+            $data['error_input'] = "invalid_betalingsinstructie";
+        } elseif (!preg_match("/^[a-zA-Z0-9-]*$/", $_POST['verzendkosten'])) { //						verzendkosten
+            $data['error_input'] = "invalid_verzendkosten";
+        } elseif (!preg_match("/^[a-zA-Z0-9-]*$/", $_POST['verzendinstructies'])) { //						verzendinstructies
+            $data['error_input'] = "invalid_verzendinstructies";
+        }
+        return empty($data['error_input']);//if no errors returns true
+    }
 }
