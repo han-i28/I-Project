b6575ea686ba59f2592884bb6787ce9fa46329e1
Stijn Wingens <stijnwingens140@gmail.com>
2019-06-05 14:59:23 +0200

    Header rework

--- view/includes/header.inc.php
+++ view/includes/header.inc.php
@@ -1,31 +1,69 @@
-<nav class="uk-navbar-container" uk-navbar="dropbar: false;" uk-sticky="bottom: #offset">
-    <div class="uk-navbar-left">
-        <ul class="uk-navbar-nav uk-visible@s">
-            <li>
-                <a href="#">Account</a>
+<?php
+    if(!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] === false ) {
+        $headerAccountItemHtml = '
+        <li><a href="#">Account</a>
+            <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
+                <ul class="uk-nav uk-navbar-dropdown-nav">
+                    <li><a href="' . SITEURL . 'login">Inloggen</a></li>
+                    <li><a href="' . SITEURL . 'registreren">Registreren</a></li>
+                </ul>
+            </div>
+        </li>';
+
+        $headerAccountItemHtmlSmall = '
+        <li>
+                <a class="uk-navbar-toggle" uk-navbar-toggle-icon href="#"></a>
                 <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
                     <ul class="uk-nav uk-navbar-dropdown-nav">
-					<?php
-						if(!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] === false ) { ?>
-							<li><a href="<?php echo SITEURL . 'login'; ?>">Inloggen</a></li>
-							<li><a href="<?php echo SITEURL . 'registreren'; ?>">Registreren</a></li>
-					<?php } elseif (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) { ?>
-							<li><p>Welkom, <?php echo $_SESSION['gebruikersnaam']; ?></p></li>
-                            <li class="uk-active"><a href="<?php echo SITEURL . 'account'; ?>">Profiel</a></li>
-                            <li class="uk-nav-divider"></li>
-							<li><a href="<?php echo SITEURL . 'login/logout'; ?>">Uitloggen</a></li>
-					<?php } ?>
+                        <li><a href="' . SITEURL . 'login">Inloggen</a></li>
+                        <li><a href="' . SITEURL . 'registreren">Registreren</a></li>
                     </ul>
                 </div>
             </li>
+        ';
+    }
+
+    elseif (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) {
+        $headerAccountItemHtml .= '
+        <li><a href="#">Account - ' . $_SESSION['gebruikersnaam'] . '</a>
+            <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
+                <ul class="uk-nav uk-navbar-dropdown-nav">
+                    <li><p>Welkom, ' . $_SESSION['gebruikersnaam'] . '</p></li>
+                    <li class="uk-active"><a href="' . SITEURL . 'account>Profiel</a></li>
+                    <li class="uk-nav-divider"></li>
+                    <li><a href="' . SITEURL . 'login/logout>Uitloggen</a></li>
+                </ul>
+            </div>
+        </li>';   
+        
+        $headerAccountItemHtmlSmall = '
+        <li>
+            <a class="uk-navbar-toggle" uk-navbar-toggle-icon href="#"></a>
+            <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
+                <ul class="uk-nav uk-navbar-dropdown-nav">
+                    <li><p>Welkom, ' . $_SESSION['gebruikersnaam'] . '</p></li>
+                    <li><a href="' . SITEURL . 'account">Account</a></li>
+                    <li class="uk-nav-divider"></li>
+                    <li><a href="' . SITEURL . 'login/logout">Uitloggen</a></li>
+                </ul>
+            </div>
+        </li>
+        ';
+    }
+?>
+
+<nav class="uk-navbar-container" uk-navbar="dropbar: false;" uk-sticky="bottom: #offset">
+    <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav uk-visible@s">
+            <?=$headerAccountItemHtml?>
         </ul>
     </div>
     <div class="uk-navbar-center uk-logo">
-       <a href="<?php echo SITEURL; ?>"> <img style="max-height: 80px" src="<?php echo SITEURL . "assets/images/logo.png" ?>" alt="Logo"></a>
-   </div>
+       <a href="<?=SITEURL?>"> <img style="max-height: 80px" src="<?=SITEURL?>assets/images/logo.png" alt="Logo"></a>
+    </div>
     </div>
     <div class="uk-navbar-right uk-margin-right uk-margin-top uk-visible@s">
-        <form action="<?php echo SITEURL; ?>veiling/zoekopdracht" class="uk-flex" method="GET">
+        <form action="<?=SITEURL?>veiling/zoekopdracht" class="uk-flex" method="GET">
             <input type="search" name="search" id="" class="uk-input" placeholder="Zoeken...">
             <button class="uk-button-primary zoekbalk" type="submit"><span class="uk-icon" uk-icon="icon: search"></span></button>
         </form>
@@ -40,23 +78,7 @@
     
     <div class="uk-navbar-left uk-navbar-left uk-hidden@s">
         <ul class="uk-navbar-nav uk-hidden@s">
-            <li>
-                <a class="uk-navbar-toggle" uk-navbar-toggle-icon href="#"></a>
-                <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
-                    <ul class="uk-nav uk-navbar-dropdown-nav">
-					<?php
-						if(!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] === false ) { ?>														
-							<li><a href="<?php echo SITEURL . 'login'; ?>">Inloggen</a></li>
-							<li><a href="<?php echo SITEURL . 'registreren'; ?>">Registreren</a></li>
-					<?php } elseif (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) { ?>
-                            <li><p>Welkom, <?php echo $_SESSION['gebruikersnaam']; ?></p></li>                        
-                            <li><a href="<?php echo SITEURL . 'account'; ?>">Account</a></li>
-							<li class="uk-nav-divider"></li>                        
-							<li><a href="<?php echo SITEURL . 'login/logout'; ?>">Uitloggen</a></li>
-					<?php } ?>
-                    </ul>
-                </div>
-            </li>
+            <?=$headerAccountItemHtmlSmall?>
         </ul>    
     </div>
 
@@ -66,7 +88,7 @@
 
             <div class="uk-grid-small uk-flex-middle" uk-grid>
                 <div class="uk-width-expand">
-                    <form class="uk-search uk-search-navbar uk-width-1-1" method="GET" action="<?php echo SITEURL; ?>veiling/zoekopdracht">
+                    <form class="uk-search uk-search-navbar uk-width-1-1" method="GET" action="<?=SITEURL?>veiling/zoekopdracht">
                         <input class="uk-search-input" name= "search" type="search" placeholder="Zoeken..." autofocus>
                     </form>
                 </div>
