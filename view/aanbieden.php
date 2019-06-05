36d536175672931652cd7f6fac84de95647ba20d
jasperjacobs <49537790+jasperjacobs@users.noreply.github.com>
2019-06-05 11:53:05 +0200

    Update aanbieden.php
    kleine dingen gewijzigd

--- view/aanbieden.php
+++ view/aanbieden.php
@@ -1,7 +1,7 @@
 <div class="uk-container uk-card uk-card-default uk-width-1-2@s uk-margin-top">
     <div class="uk-grid" uk-grid>
             <form action="" class="uk-form-horizontal uk-width-1-1 uk-margin-large"  method="post" enctype="multipart/form-data">
-                <h1 class="uk-margin-top uk-width-1-1 uk-text-center uk-card-title">Registratie</h1>
+                <h1 class="uk-margin-top uk-width-1-1 uk-text-center uk-card-title">Nieuwe veiling maken</h1>
                 <p class="uk-width-1-1 uk-text-center">Een nieuwe wereld van veilingen gaat voor u open!</p>
                 <?=$message?>
                 <p class="uk-width-1-1 uk-text-center"><b>Let op:</b> velden met aan de rechterzijde het <span class="uk-icon" uk-icon="icon: chevron-double-left"></span> icoon zijn verplicht.</p>
@@ -66,8 +66,19 @@
                 <div class="uk-width-1-1 uk-margin-top">
                     <label class="uk-form-label" for="verzendkosten">Eventuele verzendkosten</label>
                     <div class="uk-form-controls">
-                        <div class="uk-inline uk-width-1-1">
+                        <div class="uk-inline uk-width-1-1"> <!-- minimum van 0 moeten invoeren bij ophalen en hoger bij verzenden-->
+                            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                             &euro;<input class="uk-input" type="number" id="verzendkosten" name="verzendkosten" placeholder="Verzendkosten" value="<?= (isset($_POST['verzendkosten']) ? $_POST['verzendkosten'] : null); ?>">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="uk-width-1-1 uk-margin-top">
+                    <label class="uk-form-label" for="verzendinstructie">Eventuele Verzendinstructie</label>
+                    <div class="uk-form-controls">
+                        <div class="uk-inline uk-width-1-1">
+                            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
+                            <textarea class="uk-input" id="verzendinstructie" name="verzendinstructie" rows="5"  placeholder="Geef een eventuele verzend of ophaal instructie" value="<?= (isset($_POST['beschrijving']) ? $_POST['beschrijving'] : null); ?>"></textarea>
                         </div>
                     </div>
                 </div>
@@ -82,9 +93,9 @@
                             <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: chevron-double-left"></span>
                             <select class="uk-select" name="looptijd" id="looptijd" value="<?= (isset($_POST['looptijd']) ? $_POST['looptijd'] : null); ?>">
                                 <option selected disabled>Kies een looptijd</option>
-                                <option value="3">3</option>
-                                <option value="5">5</option>
-                                <option value="7">7</option>
+                                <option value="3">3 dagen</option>
+                                <option value="5">5 dagen</option>
+                                <option value="7">7 dagen</option>
                             </select>
                         </div>
                     </div>
@@ -101,6 +112,7 @@
                             <select class="uk-select" name="conditie" id="conditie" value="<?= (isset($_POST['conditie']) ? $_POST['conditie'] : null); ?>">
                                 <option selected disabled>Kies een conditie</option>
                                 <option value="nieuw">Nieuw</option>
+                                <option value="zo goed als nieuw">zo goed als nieuw</option>
                                 <option value="gebruikt">Gebruikt</option>
                                 <option value="beschadigd">Beschadigd, zie beschrijving</option>
                             </select>
