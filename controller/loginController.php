4307c46b6900e776b9cfd73af8a99e859f726540
Mike <48909795+mikevh17@users.noreply.github.com>
2019-06-05 15:03:19 +0200

    Update loginController.php

--- controller/loginController.php
+++ controller/loginController.php
@@ -2,39 +2,29 @@
 
 class loginController extends Controller {
     function index() {
+        session_start();
         if(!isset($_SESSION['loggedIn'])) {
             if (isset($_POST['login_submit'])) {
-                
                 $this->secure_form($_POST);
-
-                if (!defined($_POST['gebruikersnaam']) || !defined($_POST['wachtwoord'])) {
+                if (empty($_POST['gebruikersnaam']) || empty($_POST['wachtwoord'])) {
                     $data['error_input'] = "empty_fields";
-                    header("Location: ../login");
-                    exit();
                 } else {
                     require(PATH . '/model/loginModel.php');
                     $loginModel = new loginModel();
 
                     $resultArray = $loginModel->getUserAuthentication($_POST['gebruikersnaam']);
-                    if (empty($resultArray)) {
+
+                    if (empty($resultArray) && !isset($data['error_input'])) {
                         $data['error_input'] = "wrong_input";
-                        header("Location: ../login");
-                        exit();
                     } else {
                         $pwdCheck = password_verify($_POST['wachtwoord'], $resultArray['wachtwoord']);
 
                         if (!$pwdCheck) {
                             $data['error_input'] = "wrong_input";
-                            header("Location: ../login");
-                            exit();
-                        } elseif ($resultArray['is_geverifieerd'] == 0) {
+                        } elseif ($resultArray['isGeverifieerd'] == 0) {
                             $data['error_input'] = "not_verified";
-                            header("Location: ../login");
-                            exit();
-                        } else if ($pwdCheck == true && $resultArray['is_geverifieerd'] == 1) {
-
+                        } else if ($pwdCheck == true && $resultArray['isGeverifieerd'] == 1) {
                             ini_set('session.cookie_httponly', true); //Preventie van session hijacking door cookies niet door javascript te kunnen sturen
-
                             session_start();
 
                             if (isset($_SESSION['last_ip']) === false) {
@@ -69,7 +59,7 @@ class loginController extends Controller {
         session_start();
         session_unset();
         session_destroy();
-        header(SITEURL);
+        header('location: ' . SITEURL . '');
     }
 }
 
