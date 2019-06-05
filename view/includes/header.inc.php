a7832ff58c01e61cdabf09412d4791cf29c9b77b
Stijn Wingens <stijnwingens140@gmail.com>
2019-06-05 15:14:30 +0200

    fix some bugs

--- view/includes/header.inc.php
+++ view/includes/header.inc.php
@@ -1,5 +1,6 @@
 <?php
-    if(!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] === false ) {
+    $headerAccountItemHtml = '';
+    if(!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] == false ) {
         $headerAccountItemHtml = '
         <li><a href="#">Account</a>
             <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
@@ -11,34 +12,32 @@
         </li>';
 
         $headerAccountItemHtmlSmall = '
-        <li>
-                <a class="uk-navbar-toggle" uk-navbar-toggle-icon href="#"></a>
-                <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
-                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                        <li><a href="' . SITEURL . 'login">Inloggen</a></li>
-                        <li><a href="' . SITEURL . 'registreren">Registreren</a></li>
-                    </ul>
-                </div>
-            </li>
+        <li><a class="uk-navbar-toggle" uk-navbar-toggle-icon href="#"></a>
+            <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
+                <ul class="uk-nav uk-navbar-dropdown-nav">
+                    <li><a href="' . SITEURL . 'login">Inloggen</a></li>
+                    <li><a href="' . SITEURL . 'registreren">Registreren</a></li>
+                </ul>
+            </div>
+        </li>
         ';
     }
 
-    elseif (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) {
+    elseif (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
         $headerAccountItemHtml .= '
         <li><a href="#">Account - ' . $_SESSION['gebruikersnaam'] . '</a>
             <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
                 <ul class="uk-nav uk-navbar-dropdown-nav">
                     <li><p>Welkom, ' . $_SESSION['gebruikersnaam'] . '</p></li>
-                    <li class="uk-active"><a href="' . SITEURL . 'account>Profiel</a></li>
+                    <li class="uk-active"><a href="' . SITEURL . 'account">Profiel</a></li>
                     <li class="uk-nav-divider"></li>
-                    <li><a href="' . SITEURL . 'login/logout>Uitloggen</a></li>
+                    <li><a href="' . SITEURL . 'login/logout">Uitloggen</a></li>
                 </ul>
             </div>
         </li>';   
         
         $headerAccountItemHtmlSmall = '
-        <li>
-            <a class="uk-navbar-toggle" uk-navbar-toggle-icon href="#"></a>
+        <li><a class="uk-navbar-toggle" uk-navbar-toggle-icon href="#"></a>
             <div uk-dropdown="pos:bottom-justify" class="uk-navbar-dropdown">
                 <ul class="uk-nav uk-navbar-dropdown-nav">
                     <li><p>Welkom, ' . $_SESSION['gebruikersnaam'] . '</p></li>
@@ -61,18 +60,11 @@
     <div class="uk-navbar-center uk-logo">
        <a href="<?=SITEURL?>"> <img style="max-height: 80px" src="<?=SITEURL?>assets/images/logo.png" alt="Logo"></a>
     </div>
-    </div>
     <div class="uk-navbar-right uk-margin-right uk-margin-top uk-visible@s">
         <form action="<?=SITEURL?>veiling/zoekopdracht" class="uk-flex" method="GET">
             <input type="search" name="search" id="" class="uk-input" placeholder="Zoeken...">
             <button class="uk-button-primary zoekbalk" type="submit"><span class="uk-icon" uk-icon="icon: search"></span></button>
         </form>
-        <!--        <a class="uk-navbar-toggle" uk-search-icon href="#"></a>-->
-        <!--        <div class="uk-drop" uk-drop="mode: click; pos: left-center; offset: -20">-->
-        <!--            <form class="uk-search uk-search-navbar uk-width-1-1">-->
-        <!--                <input class="uk-search-input" type="search" placeholder="Search..." autofocus>-->
-        <!--            </form>-->
-        <!--        </div>-->
     </div>
     
     
