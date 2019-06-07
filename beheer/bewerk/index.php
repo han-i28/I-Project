9c23913e1f938853b8bf648ff81c555f965e3760
IvanTeunesen <ivanteunesen@hotmail.com>
2019-06-07 09:25:19 +0200

    Beheer fix

--- beheer/bewerk/index.php
+++ beheer/bewerk/index.php
@@ -1,3 +1,6 @@
 <?php
-require($_SERVER['DOCUMENT_ROOT'] . '/I-Project/index.php');
+    if(file_exists($_SERVER['DOCUMENT_ROOT'].'/I-Project/localSettings.php')){//local
+        require_once($_SERVER['DOCUMENT_ROOT'].'/I-Project/definedVariables.php');
+    }else require_once($_SERVER['DOCUMENT_ROOT'].'/definedVariables.php');
+    require(PATH.'/index.php');
 ?>
